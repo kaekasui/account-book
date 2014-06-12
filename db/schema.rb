1ca8369986f61b20f242a1cef1517c9ef0d607a7
@kae_kasui <kaekasui_github@icloud.com>
2014-06-13 01:38:29 +0900

    refs #93 'add acts_as_paranoid to the user model'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140609172929) do
+ActiveRecord::Schema.define(version: 20140612163305) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140609172929) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "deleted_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
