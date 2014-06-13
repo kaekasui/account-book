498961474adadf3cd97645151a94ece07c43bf5c
@kae_kasui <kaekasui_github@icloud.com>
2014-06-13 14:00:52 +0900

    refs #70 'internationalization and display the error messages'

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module AccountBook
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Tokyo'
     config.assets.enabled = false
+    config.assets.initialize_on_precompile = false
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
