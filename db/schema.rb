b922edfa166cbe1835055e3469a4edbe1a79ea3d
@kae_kasui <kaekasui_github@icloud.com>
2014-06-14 05:24:40 +0900

    refs #98 'create migrate file, and rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140613055935) do
+ActiveRecord::Schema.define(version: 20140613202111) do
+
+  create_table "breakdowns", force: true do |t|
+    t.string   "name"
+    t.integer  "category_id"
+    t.integer  "user_id"
+    t.datetime "deleted_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "categories", force: true do |t|
     t.string   "name"
