101a82418ee44fcdfdf79d472c19fe8b3ee4bcd7
@kae_kasui <kaekasui_github@icloud.com>
2014-06-13 23:54:28 +0900

    refs #96 'add model spec and controller spec about the category management'

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe CategoriesController, type: :controller do
 
   describe "GET index" do
     it "assigns all categories as @categories" do
-      category = Category.create! valid_attributes
-      get :index, {}, valid_session
+      category = create(:category)
+      get :index
       expect(assigns(:categories)).to eq([category])
     end
   end
@@ -26,31 +26,38 @@ RSpec.describe CategoriesController, type: :controller do
       get :edit, {:id => category.to_param}, valid_session
       expect(assigns(:category)).to eq(category)
     end
+
+    it "assings the requested category as @category" do
+      category = create(:category)
+      get :edit, { id: category.id }
+      expect(assigns(:category)).to eq(category)
+    end
   end
 
   describe "POST create" do
     describe "with valid params" do
       it "creates a new Category" do
         expect {
-          post :create, {:category => valid_attributes}, valid_session
+          post :create, { category: attributes_for(:category), format: :js }
         }.to change(Category, :count).by(1)
       end
 
       it "assigns a newly created category as @category" do
-        post :create, {:category => valid_attributes}, valid_session
+        post :create, { category: attributes_for(:category), format: :js }
         expect(assigns(:category)).to be_a(Category)
         expect(assigns(:category)).to be_persisted
       end
 
-      it "redirects to the created category" do
-        post :create, {:category => valid_attributes}, valid_session
-        expect(response).to redirect_to(Category.last)
-      end
+      # レスポンスステータスが200であること
+      it "has response status code, 200" do
+        post :create, { category: attributes_for(:category), format: :js }
+        expect(response.status).to eq(200)
+      end 
     end
 
     describe "with invalid params" do
       it "assigns a newly created but unsaved category as @category" do
-        post :create, {:category => invalid_attributes}, valid_session
+        post :create, { category: attributes_for(:category, name: ""), format: :js }
         expect(assigns(:category)).to be_a_new(Category)
       end
 
@@ -104,15 +111,15 @@ RSpec.describe CategoriesController, type: :controller do
 
   describe "DELETE destroy" do
     it "destroys the requested category" do
-      category = Category.create! valid_attributes
+      category = create(:category)
       expect {
-        delete :destroy, {:id => category.to_param}, valid_session
+        delete :destroy, { id: category.id }
       }.to change(Category, :count).by(-1)
     end
 
     it "redirects to the categories list" do
-      category = Category.create! valid_attributes
-      delete :destroy, {:id => category.to_param}, valid_session
+      category = create(:category)
+      delete :destroy, { id: category.id }
       expect(response).to redirect_to(categories_url)
     end
   end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+
+  # カテゴリ名が無ければ無効な状態であること
+  it 'is invalid without a category name.' do
+    category = build(:category, name: "")
+    expect(category).to be_invalid
+  end
+
+  # カテゴリ名が有れば有効な情報であること
+  it 'is valid with a category name.' do
+    category = build(:category)
+    expect(category).to be_valid
+  end
 end
