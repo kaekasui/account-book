c21c0da544c9531dd84074a0f0e54b1f220cd898
@kae_kasui <kaekasui_github@icloud.com>
2014-06-14 05:17:42 +0900

    refs #96 'limit the maximum characters about the category name'

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: MAX_LONG_TEXT_FIELD_LENGTH }
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe Category, type: :model do
     category = build(:category)
     expect(category).to be_valid
   end
+
+  # カテゴリ名が101文字であれば無効な状態であること
+  it 'is invalid with 101 characters category name' do
+    category = build(:category, name: "あ" * 101)
+    expect(category).to be_invalid
+  end
+
+  # カテゴリ名が100文字であれば有効な状態であること
+  it 'is valid with 100 characters category name' do
+    category = build(:category, name: "あ" * 100)
+    expect(category).to be_valid
+  end
 end
