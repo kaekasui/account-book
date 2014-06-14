b39aa61c21861e30e9308bb50fe1240bab551e62
@kae_kasui <kaekasui_github@icloud.com>
2014-06-14 14:38:12 +0900

    refs #101 'create a function that create, update and delete the records'

---
+++ spec/models/record_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Record, type: :model do
+  let(:user) { create(:user) }
+  let(:category) { create(:category) }
+  let(:breakdown) { create(:breakdown, category_id: category.id, user_id: user.id) }
+
+  it 'is valid with a charge and breakdown' do
+    record = build(:record, breakdown_id: breakdown.id)
+    expect(record).to be_valid
+  end
+
+  it 'is invalid without a charge' do
+    record = build(:record, charge: "", breakdown_id: breakdown.id)
+    expect(record).to be_invalid
+  end
+
+  it 'is invalid without a breakdown' do
+    record = build(:record)
+    expect(record).to be_invalid
+  end
+
+  it 'is invalid without a published date' do
+    record = build(:record, published_at: '')
+    expect(record).to be_invalid
+  end
+
+  # 論理削除となり、削除時刻が更新されること
+  it 'updates the deleted_at' do
+    record = create(:record, breakdown_id: breakdown.id)
+    expect(record.deleted_at).to be_nil
+    record.destroy
+    expect(record.deleted_at).not_to be_nil
+  end
+end
