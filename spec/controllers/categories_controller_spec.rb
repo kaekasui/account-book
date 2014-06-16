b2f323c7ab4593ae171d0eb6fb9938845cb60a83
@kae_kasui <kaekasui_github@icloud.com>
2014-06-16 21:07:31 +0900

    refs #115 'add controller specs about authenticate'

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -3,114 +3,129 @@ require 'rails_helper'
 RSpec.describe CategoriesController, type: :controller do
   let(:user) { create(:user) }
 
-  before do
-    sign_in user
-  end
-
-  describe "GET index" do
-    it "assigns all categories as @categories" do
-      category = create(:category)
-      get :index
-      expect(assigns(:categories)).to eq([category])
-    end
-  end
-
-  describe "GET edit" do
-    it "assings the requested category as @category" do
-      category = create(:category)
-      get :edit, { id: category.id }
-      expect(assigns(:category)).to eq(category)
-    end
-  end
-
-  describe "POST create" do
-    describe "with valid params" do
-      it "creates a new Category" do
-        expect {
-          post :create, { category: attributes_for(:category), format: :js }
-        }.to change(Category, :count).by(1)
-      end
-
-      it "assigns a newly created category as @category" do
-        post :create, { category: attributes_for(:category), format: :js }
-        expect(assigns(:category)).to be_a(Category)
-        expect(assigns(:category)).to be_persisted
-      end
-
-      # レスポンスステータスが200であること
-      it "has response status code, 200" do
-        post :create, { category: attributes_for(:category), format: :js }
-        expect(response.status).to eq(200)
-      end 
+  context 'with a logged-in users' do
+    before do
+      sign_in user
     end
 
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved category as @category" do
-        post :create, { category: attributes_for(:category, name: ""), format: :js }
-        expect(assigns(:category)).to be_a_new(Category)
-      end
-
-      it "re-renders the 'new' template"
-    end
-  end
-
-  describe "PUT update" do
-    describe "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested category" do
+    describe "GET index" do
+      it "assigns all categories as @categories" do
         category = create(:category)
-        put :update, { id: category.id, category: attributes_for(:category, name: "new") }
-        category.reload
-        expect(category.name).to eq("new")
+        get :index
+        expect(assigns(:categories)).to eq([category])
       end
+    end
 
-      it "assigns the requested category as @category" do
+    describe "GET edit" do
+      it "assings the requested category as @category" do
         category = create(:category)
-        put :update, { id: category.id, category: attributes_for(:category, name: "new") }
+        get :edit, { id: category.id }
         expect(assigns(:category)).to eq(category)
+      end
+    end
+
+    describe "POST create" do
+      describe "with valid params" do
+        it "creates a new Category" do
+          expect {
+            post :create, { category: attributes_for(:category), format: :js }
+          }.to change(Category, :count).by(1)
+        end
+
+        it "assigns a newly created category as @category" do
+          post :create, { category: attributes_for(:category), format: :js }
+          expect(assigns(:category)).to be_a(Category)
+          expect(assigns(:category)).to be_persisted
+        end
+
+        # レスポンスステータスが200であること
+	it "has response status code, 200" do
+          post :create, { category: attributes_for(:category), format: :js }
+          expect(response.status).to eq(200)
+        end 
+      end
+
+      describe "with invalid params" do
+        it "assigns a newly created but unsaved category as @category" do
+          post :create, { category: attributes_for(:category, name: ""), format: :js }
+          expect(assigns(:category)).to be_a_new(Category)
+        end
+
+        it "re-renders the 'new' template"
+      end
+    end
+
+    describe "PUT update" do
+      describe "with valid params" do
+        it "updates the requested category" do
+          category = create(:category)
+          put :update, { id: category.id, category: attributes_for(:category, name: "new") }
+          category.reload
+          expect(category.name).to eq("new")
+        end
+
+        it "assigns the requested category as @category" do
+          category = create(:category)
+          put :update, { id: category.id, category: attributes_for(:category, name: "new") }
+          expect(assigns(:category)).to eq(category)
+        end
+
+        it "redirects to the categories list" do
+          category = create(:category)
+          put :update, { id: category.id, category: attributes_for(:category, name: "new") }
+          expect(response).to redirect_to(categories_path)
+        end
+      end
+
+      describe "with invalid params" do
+        it "assigns the category as @category" do
+          category = create(:category)
+          put :update, { id: category.id, category: attributes_for(:category, name: "") }
+          expect(assigns(:category)).to eq(category)
+        end
+
+        it "re-renders the 'edit' template" do
+          category = create(:category)
+          put :update, { id: category.id, category: attributes_for(:category, name: "") }
+          expect(response).to render_template("edit")
+        end
+      end
+    end
+
+    describe "DELETE destroy" do
+      it "destroys the requested category" do
+        category = create(:category)
+        expect {
+          delete :destroy, { id: category.id }
+        }.to change(Category, :count).by(-1)
       end
 
       it "redirects to the categories list" do
         category = create(:category)
-        put :update, { id: category.id, category: attributes_for(:category, name: "new") }
-        expect(response).to redirect_to(categories_path)
-      end
-    end
-
-    describe "with invalid params" do
-      it "assigns the category as @category" do
-        category = create(:category)
-        put :update, { id: category.id, category: attributes_for(:category, name: "") }
-        expect(assigns(:category)).to eq(category)
-      end
-
-      it "re-renders the 'edit' template" do
-        category = create(:category)
-        put :update, { id: category.id, category: attributes_for(:category, name: "") }
-        expect(response).to render_template("edit")
-      end
-    end
-  end
-
-  describe "DELETE destroy" do
-    it "destroys the requested category" do
-      category = create(:category)
-      expect {
         delete :destroy, { id: category.id }
-      }.to change(Category, :count).by(-1)
+        expect(response).to redirect_to(categories_url)
+      end
     end
 
-    it "redirects to the categories list" do
-      category = create(:category)
-      delete :destroy, { id: category.id }
-      expect(response).to redirect_to(categories_url)
+    after do
+      sign_out :user
     end
   end
 
-  after do
-    sign_out :user
+  context 'with a non-logged-in users' do
+    describe "GET index" do
+      it "redirects to sign in page." do
+        get :index
+        expect(response).to redirect_to(user_session_path)
+      end
+    end
+
+    describe "GET edit" do
+      it "assings the requested category as @category" do
+        category = create(:category)
+        get :edit, { id: category.id }
+        expect(response).to redirect_to(user_session_path)
+      end
+    end
   end
 end
--- spec/controllers/records_controller_spec.rb
+++ spec/controllers/records_controller_spec.rb
@@ -1,145 +1,157 @@
 require 'rails_helper'
 
 RSpec.describe RecordsController, type: :controller do
-
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  let(:valid_session) { {} }
   let(:user) { create(:user) }
   let(:category) { create(:category) }
   let(:breakdown) { create(:breakdown, user_id: user.id, category_id: category.id) }
 
-  before do
-    sign_in user
-  end
-
-  describe "GET index" do
-    it "assigns all records as @records" do
-      record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
-      get :index
-      expect(assigns(:records)).to eq([record])
-    end
-  end
-
-  describe "GET show" do
-    it "assigns the requested record as @record" do
-      record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
-      get :show, { id: record.id }
-      expect(assigns(:record)).to eq(record)
-    end
-  end
-
-  describe "GET new" do
-    it "assigns a new record as @record" do
-      get :new
-      expect(assigns(:record)).to be_a_new(Record)
-    end
-  end
-
-  describe "GET edit" do
-    it "assigns the requested record as @record" do
-      record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
-      get :edit, { id: record.id }
-      expect(assigns(:record)).to eq(record)
-    end
-  end
-
-  describe "POST create" do
-    describe "with valid params" do
-      it "creates a new Record" do
-        expect {
-          post :create, { record: attributes_for(:record, breakdown_id: breakdown.id, user_id: user.id) }
-        }.to change(Record, :count).by(1)
-      end
-
-      it "assigns a newly created record as @record" do
-        post :create, { record: attributes_for(:record, breakdown_id: breakdown.id, user_id: user.id) }
-        expect(assigns(:record)).to be_a(Record)
-        expect(assigns(:record)).to be_persisted
-      end
-
-      it "redirects to the created record" do
-        post :create, { record: attributes_for(:record, breakdown_id: breakdown.id, user_id: user.id) }
-        expect(response).to redirect_to(Record.last)
-      end
+  context 'with a logged-in user' do
+    before do
+      sign_in user
     end
 
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved record as @record" do
-        post :create, { record: attributes_for(:record) }
-        expect(assigns(:record)).to be_a_new(Record)
-      end
-
-      it "re-renders the 'new' template" do
-        post :create, { record: attributes_for(:record) }
-        expect(response).to render_template("new")
-      end
-    end
-  end
-
-  describe "PUT update" do
-    describe "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested record" do
+    describe "GET index" do
+      it "assigns all records as @records" do
         record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
-        put :update, { id: record.id, record: attributes_for(:record, charge: 2000, breakdown_id: breakdown.id, user_id: user.id) }
-        record.reload
-        expect(record.charge).to eq 2000
+        get :index
+        expect(assigns(:records)).to eq([record])
       end
+    end
 
+    describe "GET show" do
       it "assigns the requested record as @record" do
         record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
-        put :update, { id: record.id, record: attributes_for(:record, breakdown_id: breakdown.id, user_id: user.id) }
+        get :show, { id: record.id }
         expect(assigns(:record)).to eq(record)
-      end
-
-      it "redirects to the record" do
-        record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
-        put :update, { id: record.id, record: attributes_for(:record, breakdown_id: breakdown.id, user_id: user.id) }
-        expect(response).to redirect_to(record)
       end
     end
 
-    describe "with invalid params" do
-      it "assigns the record as @record" do
-        record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
-        put :update, { id: record.id, record: attributes_for(:record, charge: '') }
-        expect(assigns(:record)).to eq(record)
-      end
-
-      it "re-renders the 'edit' template" do
-        record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
-        put :update, { id: record.id, record: attributes_for(:record, charge: '') }
-        expect(response).to render_template("edit")
+    describe "GET new" do
+      it "assigns a new record as @record" do
+        get :new
+        expect(assigns(:record)).to be_a_new(Record)
       end
     end
-  end
 
-  describe "DELETE destroy" do
-    it "destroys the requested record" do
-      record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
-      expect {
+    describe "GET edit" do
+      it "assigns the requested record as @record" do
+        record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
+        get :edit, { id: record.id }
+        expect(assigns(:record)).to eq(record)
+      end
+    end
+
+    describe "POST create" do
+      describe "with valid params" do
+        it "creates a new Record" do
+          expect {
+            post :create, { record: attributes_for(:record, breakdown_id: breakdown.id, user_id: user.id) }
+          }.to change(Record, :count).by(1)
+        end
+
+        it "assigns a newly created record as @record" do
+          post :create, { record: attributes_for(:record, breakdown_id: breakdown.id, user_id: user.id) }
+          expect(assigns(:record)).to be_a(Record)
+          expect(assigns(:record)).to be_persisted
+        end
+
+        it "redirects to the created record" do
+          post :create, { record: attributes_for(:record, breakdown_id: breakdown.id, user_id: user.id) }
+          expect(response).to redirect_to(Record.last)
+        end
+      end
+
+      describe "with invalid params" do
+        it "assigns a newly created but unsaved record as @record" do
+          post :create, { record: attributes_for(:record) }
+          expect(assigns(:record)).to be_a_new(Record)
+        end
+
+        it "re-renders the 'new' template" do
+          post :create, { record: attributes_for(:record) }
+          expect(response).to render_template("new")
+        end
+      end
+    end
+
+    describe "PUT update" do
+      describe "with valid params" do
+        it "updates the requested record" do
+          record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
+          put :update, { id: record.id, record: attributes_for(:record, charge: 2000, breakdown_id: breakdown.id, user_id: user.id) }
+          record.reload
+          expect(record.charge).to eq 2000
+        end
+
+        it "assigns the requested record as @record" do
+          record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
+          put :update, { id: record.id, record: attributes_for(:record, breakdown_id: breakdown.id, user_id: user.id) }
+          expect(assigns(:record)).to eq(record)
+        end
+
+        it "redirects to the record" do
+          record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
+          put :update, { id: record.id, record: attributes_for(:record, breakdown_id: breakdown.id, user_id: user.id) }
+          expect(response).to redirect_to(record)
+        end
+      end
+
+      describe "with invalid params" do
+        it "assigns the record as @record" do
+          record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
+          put :update, { id: record.id, record: attributes_for(:record, charge: '') }
+          expect(assigns(:record)).to eq(record)
+        end
+
+        it "re-renders the 'edit' template" do
+          record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
+          put :update, { id: record.id, record: attributes_for(:record, charge: '') }
+          expect(response).to render_template("edit")
+        end
+      end
+    end
+
+    describe "DELETE destroy" do
+      it "destroys the requested record" do
+        record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
+        expect {
+          delete :destroy, { id: record.id }
+        }.to change(Record, :count).by(-1)
+      end
+
+      it "redirects to the records list" do
+        record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
         delete :destroy, { id: record.id }
-      }.to change(Record, :count).by(-1)
+        expect(response).to redirect_to(records_url)
+      end
     end
 
-    it "redirects to the records list" do
-      record = create(:record, breakdown_id: breakdown.id, user_id: user.id)
-      delete :destroy, { id: record.id }
-      expect(response).to redirect_to(records_url)
+    after do
+      sign_out :user
     end
   end
 
-  after do
-    sign_out :user
+  context 'with a non-logged-in user' do
+    describe "GET index" do
+      it "redirects to sign in page." do
+        get :index
+        expect(response).to redirect_to(user_session_path)
+      end
+    end
+
+    describe "GET new" do
+      it "redirects to sign in page." do
+        get :new
+        expect(response).to redirect_to(user_session_path)
+      end
+    end
+
+    describe "GET edit" do
+      it "redirects to sign in page." do
+        category = create(:category)
+        get :edit, { id: category.id }
+        expect(response).to redirect_to(user_session_path)
+      end
+    end
   end
 end
