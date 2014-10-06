5a3af8faf55bc77aaba5722ef70399dd3b6a249c
m-kojima <aquariuslake@gmail.com>
2014-10-06 11:25:45 +0900

    refs #73 add the rspec files about twitter users

--- spec/features/twitter_users_spec.rb
+++ spec/features/twitter_users_spec.rb
@@ -8,6 +8,18 @@ feature 'Twitterアカウントの管理' do
     expect(page).to have_content I18n.t("devise.omniauth_callbacks.success", kind: 'twitter')
   end
 
+  scenario 'ログインする(2回目)' do
+    visit "/users/auth/twitter"
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content I18n.t("devise.omniauth_callbacks.success", kind: 'twitter')
+    click_link I18n.t("links.sign_out")
+
+    visit "/users/auth/twitter"
+    expect(current_path).to eq root_path
+    expect(page).to have_content I18n.t("devise.omniauth_callbacks.success", kind: 'twitter')
+  end
+
   scenario 'ログアウトする' do
     visit "/users/auth/twitter"
 
