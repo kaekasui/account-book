3eb814a608d44734cc139de43d12e638e3b96611
m-kojima <aquariuslake@gmail.com>
2014-10-12 14:28:01 +0900

    refs #556 add the page about feedbacks

--- app/controllers/admin/feedbacks_controller.rb
+++ app/controllers/admin/feedbacks_controller.rb
@@ -8,36 +8,36 @@ class Admin::FeedbacksController < Admin::AdminBaseController
   end
 
   def show
-    respond_with(@feedback)
+    respond_with(@admin_feedback)
   end
 
   def new
-    @feedback = Feedback.new
-    respond_with(@feedback)
+    @admin_feedback = Feedback.new
+    respond_with(@admin_feedback)
   end
 
   def edit
   end
 
   def create
-    @feedback = Feedback.new(feedback_params)
+    @admin_feedback = Feedback.new(feedback_params)
     @admin_feedback.save
-    respond_with(@feedback)
+    respond_with(@admin_feedback)
   end
 
   def update
     @admin_feedback.update(feedback_params)
-    respond_with(@feedback)
+    respond_with(@admin_feedback)
   end
 
   def destroy
     @admin_feedback.destroy
-    respond_with(@feedback)
+    respond_with(@admin_feedback)
   end
 
   private
     def set_feedback
-      @feedback = Feedback.find(params[:id])
+      @admin_feedback = Feedback.find(params[:id])
     end
 
     def feedback_params
---
+++ spec/controllers/admin/feedbacks_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Admin::FeedbacksController, type: :controller do
+  let(:user) { create(:user, confirmed_at: Time.now, admin: true) }
+
+  context 'ユーザーがログインしている場合' do
+    before do
+      sign_in user
+    end
+
+    describe "一覧画面" do
+      it "フィードバックの一覧が表示されること" do
+        feedback = create(:feedback, user_id: user.id)
+        get :index
+        expect(assigns(:admin_feedbacks)).to eq([feedback])
+      end
+    end
+
+    after do
+      sign_out :user
+    end
+  end
+end
