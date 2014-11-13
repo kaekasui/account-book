b238ae784bc16ba8dbc2b990bef986c8fe5e5ebe
m-kojima <aquariuslake@gmail.com>
2014-11-14 07:37:00 +0900

    refs #212 add the categories and breakdowns data with sidekiq

---
+++ app/workers/create_categories_and_breakdowns_worker.rb
@@ -0,0 +1,9 @@
+class CreateCategoriesAndBreakdownsWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: :sidekiq, retry: 3
+
+  def perform(id)
+    puts "*** user_id: #{id}"
+    User.where(id: id).first.create_data
+  end
+end
