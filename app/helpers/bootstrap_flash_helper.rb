c046a8aa6e95e6efece5e1b68b3c602684c4d0ec
m-kojima <aquariuslake@gmail.com>
2014-11-21 05:57:33 +0900

    refs #713 modified the helper file by rubocop

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -1,18 +1,38 @@
+# Boostrap relationship modules
 module BootstrapFlashHelper
-  ALERT_TYPES = [:error, :info, :success, :warning] unless const_defined?(:ALERT_TYPES)
+  unless const_defined?(:ALERT_TYPES)
+    ALERT_TYPES = [:error, :info, :success, :warning]
+  end
 
   def bootstrap_flash
     flash_messages = []
     flash.each do |type, message|
       next if message.blank?
-      type = :success if type == :notice
-      type = :error if type == :alert
+      type = change_type(type)
       next unless ALERT_TYPES.include?(type)
       Array(message).each do |msg|
-        text = content_tag(:div, content_tag(:button, raw('&times;'), :class => 'close', 'data-dismiss' => 'alert', 'aria-hidden' => true) + msg.html_safe, :class => 'alert alert-dismissable alert-danger')
-        flash_messages << text if msg
+        flash_messages << build_tag(msg) if msg
       end
     end
     flash_messages.join('\n').html_safe
+  end
+
+  private
+
+  def change_type(type)
+    :success if type == :notice
+    :error if type == :alert
+  end
+
+  def build_tag(msg)
+    content_tag(
+      :div,
+      content_tag(:button,
+                  raw('&times;'),
+                  :class => 'close',
+                  'data-dismiss' => 'alert',
+                  'aria-hidden' => true
+      ) + msg.html_safe,
+      class: 'alert alert-dismissable alert-danger')
   end
 end
