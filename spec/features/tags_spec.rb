2264bebdc7c4a2c7d87d18b8fe94d2ab989f2c9a
m-kojima <aquariuslake@gmail.com>
2014-11-21 03:57:23 +0900

    refs #728 add a features spec for the labels

---
+++ spec/features/tags_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'Tag' do
+  let(:user) { create(:user, confirmed_at: Time.now) }
+  let(:category) { create(:category, user_id: user.id) }
+  let(:record) { create(:record, category_id: category.id, user_id: user.id, published_at: Time.now) }
+
+  background do
+    login user
+  end
+
+  scenario '履歴一覧にラベルが表示されること' do
+    visit edit_record_path(id: record.id)
+    fill_in 'record_tagged', with: "ラベル"
+    click_button I18n.t("buttons.update")
+
+    visit records_path
+    expect(page).to have_selector(".glyphicon.glyphicon-bookmark[title=ラベル]")
+  end
+end
