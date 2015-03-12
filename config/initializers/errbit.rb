bde7bb82c45ade2a71402d3d7ba88b47f2d6997b
m-kojima <aquariuslake@gmail.com>
2015-03-12 11:42:26 +0900

    set the environment variable for the errbit

---
+++ config/initializers/errbit.rb
@@ -0,0 +1,18 @@
+# Require the airbrake gem in your App.
+# ---------------------------------------------
+#
+# Rails 3 - In your Gemfile
+# gem 'airbrake'
+#
+# Rails 2 - In environment.rb
+# config.gem 'airbrake'
+#
+# Then add the following to config/initializers/errbit.rb
+# -------------------------------------------------------
+
+Airbrake.configure do |config|
+  config.api_key = '81dc3f70f85b15a1b400afe638ff39ad'
+  config.host    = 'summer-snowflake-errbit.herokuapp.com'
+  config.port    = 443
+  config.secure  = config.port == 443
+end
