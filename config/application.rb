1f4c5302ca4c149677d60ae1a8789b025302e38a
m-kojima <aquariuslake@gmail.com>
2015-03-25 16:13:03 +0900

    add the rule that is compact/nest and arrow the japanese

--- config/application.rb
+++ config/application.rb
@@ -6,22 +6,20 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module AccountBook
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+class AccountBook::Application < Rails::Application
+  # Settings in config/environments/* take precedence over those specified here.
+  # Application configuration should go into files in config/initializers
+  # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-    config.time_zone = 'Tokyo'
-    config.assets.enabled = false
-    config.assets.initialize_on_precompile = false
+  # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+  # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+  # config.time_zone = 'Central Time (US & Canada)'
+  config.time_zone = 'Tokyo'
+  config.assets.enabled = false
+  config.assets.initialize_on_precompile = false
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-    config.i18n.default_locale = :ja
-  end
+  # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+  # config.i18n.default_locale = :de
+  config.i18n.default_locale = :ja
 end
